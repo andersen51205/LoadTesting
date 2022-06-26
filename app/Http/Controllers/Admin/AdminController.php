8d463b887a07d068711ee0cb7a9396fbd152cae3
andersen <andersen51205@gmail.com>
2022-06-26 21:42:46 +0800

    Admin 帳號資訊 完成

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,19 +3,31 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Validators\User\UserInformationValidator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Auth;
 
 class AdminController extends Controller
 {
+    protected $user;
+    protected $userInformationValidator;
+    
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(
+        User $user,
+        UserInformationValidator $userInformationValidator
+    )
     {
         // 已透過Middleware進行檢查
         // $this->middleware('auth');
+        $this->user = $user;
+        $this->userInformationValidator = $userInformationValidator;
     }
 
     /**
@@ -26,5 +38,58 @@ class AdminController extends Controller
     public function main()
     {
         return view('Admin.AdminMain');
+    }
+
+    public function infomation()
+    {
+        // Get Data
+        $data = $this->user->where('id', Auth::user()->id)
+                           ->first();
+        // Response
+        return view('Admin.UserInformationMain', compact('data'));
+    }
+
+    public function update(Request $request)
+    {
+        // Validate
+        $this->userInformationValidator->checkUserInformation($request);
+        // Check Password
+        if($request['password']) {
+            $user = Auth::user();
+            if (!Hash::check($request['currentPassword'], $user['password'])) {
+                return response('Current Password Wrong', 200);
+            }
+            else if ($request['password'] !== $request['confirmPassword']) {
+                return response('Confirm Password Failed', 200);
+            }
+            else {
+                $newpassword = bcrypt($request['password']);
+            }
+        }
+        // Get Data
+        $data = ['name' => $request['name']];
+        if(isset($newpassword)) {
+            $data["password"] = $newpassword;
+        }
+        // Update
+        try {
+            $this->user->where('email', Auth::user()->email)
+                       ->update($data);
+            $result = [
+                'result' => 'successful'
+            ];
+        } catch (\Exception $e) {
+            $result = [
+                'result' => 'failure',
+                'message' => $e->getMessage()];
+        }   
+        // Response
+        if ($result['result'] === 'successful') {
+            return response('Successful', 200);
+        }
+        elseif ($result['result'] === 'failure') {
+            return response('Server Error', 500);
+        }
+        return response('Bad Request', 400);
     }
 }
