17ccc11aa81718babe4b87651269dfc79acdd89e
andersen <andersen51205@gmail.com>
2022-07-01 17:20:18 +0800

    電子郵件驗證功能 完成

--- app/Http/Controllers/Auth/VerifyController.php
+++ app/Http/Controllers/Auth/VerifyController.php
@@ -36,6 +36,45 @@ class VerifyController extends Controller
 
     public function verify(Request $request)
     {
-        //
+        // Get User
+        $userModel = Auth::user();
+        // Get Authentication Code
+        $authCode= (int) $request['authenticationCode'];
+        $currentAuthCode = Cache::get($userModel['email']);
+        // Check Authentication Code
+        if($authCode !== $currentAuthCode) {
+            $message = '認證碼錯誤，請重新確認';
+            return response()->json([
+                'message' => $message,
+            ], 401);
+        }
+        // Update User Data
+        $userModel->update([
+            'email_verified_at' => Carbon::now()
+        ]);
+        // Clear Cache
+        Cache::forget($userModel['email']);
+        // 一般使用者
+        if(Auth::user()->permission === 1) {
+            return response()->json([
+                'redirectTarget' => route('User_View')
+            ], 200);
+        }
+        // 一般管理員
+        elseif(Auth::user()->permission === 2) {
+            return response()->json([
+                'redirectTarget' => route('Manager_View')
+            ], 200);
+        }
+        // 最高管理員
+        elseif(Auth::user()->permission === 3) {
+            return response()->json([
+                'redirectTarget' => route('Admin_View')
+            ], 200);
+        }
+        // 其他
+        return response()->json([
+            'redirectTarget' => route('Login_View')
+        ], 200);
     }
 }
